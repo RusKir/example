198d544e270287854543d308e52bf4cddb303cd7
ruslankiryushkin <ruslan-x-box@mail.ru>
2025-10-03 16:29:34 +0300

    chore: initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.service;
+
+import gorg.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+@Test
+    public void ShouldReturn0IfAmount1000() {
+    CashbackHackService service = new CashbackHackService();
+    int amount = 1000;
+    int actual = service.remain(amount);
+    int expected = 0;
+    assertEquals(expected, actual);
+
+}
+}
